bdc2973c3fb8c9be054dd9bb295071e3551c5d92
Marvin Liu <marvinliu9@gmail.com>
2016-09-17 03:24:19 -0400

    add subscription support

--- run.py
+++ run.py
@@ -1,33 +1,49 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, g
 import twilio.twiml
 import geocode
 import functions
+import sqlite3
+
+DATABASE = 'forecast'
 
 app = Flask(__name__)
 
-callers = {
-    "+19496488407": "Dalvor"
-}
-
 @app.route("/", methods=['GET', 'POST'])
 def respond():
-    """Respond and greet the caller by name."""
-
     from_number = request.values.get('From', None)
     text_body = request.values.get('Body', None)
 
-    if text_body == "seven day forecast" or text_body == None:
+    if text_body == "seven day forecast":
         areaCode = '310'
         if from_number != None:
             areaCode = from_number[2:5]
         coord = geocode.getCoordinates(areaCode)
         message = functions.sevenDayForecast(coord[0], coord[1])
+    elif text_body == None:
+        crop = "lol"
+        from_number = "+19493953324"
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.execute('SELECT COUNT(*) from subscription')
+        (count,) = cursor.fetchone()
+        insertQuery = "INSERT INTO subscription VALUES(%i, \'%s\', \'%s\')" % (count, from_number, crop)
+        conn.execute(insertQuery)
+        conn.commit()
+        conn.close()
+        message = "You have subscribed to updates for " + insertQuery + "!"
     else:
-        areaCode = '310'
-        if from_number != None:
-            areaCode = from_number[2:5]
-        coord = geocode.getCoordinates(areaCode)
-        message = "You are from: (%f, %f)" % (coord[0], coord[1])
+        args = text_body.split()
+        if args[0] == "subscribe:" and len(args) == 2:
+            crop = args[1]
+            conn = sqlite3.connect(DATABASE)
+            cursor = conn.execute('SELECT COUNT(*) from subscription')
+            (count,) = cursor.fetchone()
+            insertQuery = "INSERT INTO subscription VALUES(%i, \'%s\', \'%s\')" % (count, from_number, crop)
+            conn.execute(insertQuery)
+            conn.commit()
+            conn.close()
+            message = "You have subscribed to updates for " + crop + "!"
+        else:
+            message = "Sorry, we don't recognize your text"
 
     resp = twilio.twiml.Response()
     resp.message(message)
