f4b2d9af9cc2dc68d2a5b7921063f60524c36113
Marvin Liu <marvinliu9@gmail.com>
2016-09-17 04:35:16 -0400

    add task that is scheduled every few seconds

--- run.py
+++ run.py
@@ -3,10 +3,38 @@ import twilio.twiml
 import geocode
 import functions
 import sqlite3
+from twilio.rest import TwilioRestClient
+import time
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
-DATABASE = 'forecast'
+# ===================++BACKGROUND===============================================
+account_sid = "AC069bbc61f7db2e120ba0fff6fc342e6e"
+auth_token = "44dae430182496def82f6992a64de198"
+client = TwilioRestClient(account_sid, auth_token)
+
+def send_sms():
+    #"+19496488407"
+    message = client.messages.create(to="+19493953324", from_="+13104395349",
+                                         body="dang")
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(
+    func=send_sms,
+    trigger=IntervalTrigger(seconds=5),
+    id='send_sms',
+    name='send_sms every 5 seconds',
+    replace_existing=True)
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+# ==============================================================================
 
 app = Flask(__name__)
+
+DATABASE = 'forecast'
 
 @app.route("/", methods=['GET', 'POST'])
 def respond():
@@ -14,30 +42,16 @@ def respond():
     text_body = request.values.get('Body', None)
 
     if text_body == "seven day forecast":
-        areaCode = '310'
-        if from_number != None:
-            areaCode = from_number[2:5]
-        coord = geocode.getCoordinates(areaCode)
+        coord = geocode.getCoordinates(from_number[2:5])
         message = functions.sevenDayForecast(coord[0], coord[1])
     elif text_body == None:
-        crop = "lol"
-        from_number = "+19493953324"
-        conn = sqlite3.connect(DATABASE)
-        cursor = conn.execute('SELECT COUNT(*) from subscription')
-        (count,) = cursor.fetchone()
-        insertQuery = "INSERT INTO subscription VALUES(%i, \'%s\', \'%s\')" % (count, from_number, crop)
-        conn.execute(insertQuery)
-        conn.commit()
-        conn.close()
-        message = "You have subscribed to updates for " + insertQuery + "!"
+        message = "No text body"
     else:
         args = text_body.split()
         if args[0] == "subscribe:" and len(args) == 2:
             crop = args[1]
             conn = sqlite3.connect(DATABASE)
-            cursor = conn.execute('SELECT COUNT(*) from subscription')
-            (count,) = cursor.fetchone()
-            insertQuery = "INSERT INTO subscription VALUES(%i, \'%s\', \'%s\')" % (count, from_number, crop)
+            insertQuery = "INSERT INTO subscription VALUES(\'%s\', \'%s\')" % (from_number, crop)
             conn.execute(insertQuery)
             conn.commit()
             conn.close()
