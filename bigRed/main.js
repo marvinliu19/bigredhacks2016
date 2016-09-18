6ea5475bc0a6244900d8395ed5afc51a4dfab305
Dalvor <pengcheng95@gmail.com>
2016-09-18 02:40:37 -0400

    website

---
+++ bigRed/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        var target = $(this).attr('href');
+        $('.parallax').animate({
+            scrollTop: $(target).offset().top + $('.parallax').scrollTop()
+        }, 2500);
+        return false;
+    });
+});
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function (event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+});
